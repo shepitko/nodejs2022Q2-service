c8be6c640ee727bba0d41e2111663fcb67d9e0a0
Shepitko <ya.shepitko@gmal.com>
2022-07-12 16:45:32 +0300

    feat: implement track endpoint

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateTrackDto } from './dto/create-track.dto';
+import { UpdateTrackDto } from './dto/update-track.dto';
+import { Track } from './entities/track.entity';
+import { TrackService } from './track.service';
 
 @Controller('track')
-export class TrackController {}
+export class TrackController {
+  constructor(private readonly trackService: TrackService) {}
+
+  @Get()
+  getAll(): Track[] {
+    return this.trackService.getAll();
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') trackId: string): Track {
+    return this.trackService.getOne(trackId);
+  }
+
+  @Post()
+  create(@Body() createData: CreateTrackDto) {
+    return this.trackService.create(createData);
+  }
+
+  @Put('/:id')
+  update(@Param('id') trackId: string, @Body() updateData: UpdateTrackDto) {
+    return this.trackService.update(trackId, updateData);
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id') trackId: string) {
+    return this.trackService.delete(trackId);
+  }
+}
